37d9e51fa8f8ef395ca72f9bdc003e619f2bd34f
emmanuel-nwafor <emitexc.e.o1@gmail.com>
2025-07-30 09:38:41 -0700

    updated endpoints

--- registerRoutes.js
+++ registerRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
-const { db, adminAuth, adminDb } = require('../firebaseConfig');
+const { db, adminAuth, adminDb } = require('./firebaseConfig');
 
 const generateOTP = () => Math.floor(100000 + Math.random() * 900000).toString(); // 6-digit OTP
 
