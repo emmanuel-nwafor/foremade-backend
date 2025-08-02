432d6ea707e9fd9c887bd90c870ab0fab307cd99
emmanuel-nwafor <emitexc.e.o1@gmail.com>
2025-08-01 20:10:03 -0700

    updated middleware

--- middleware.js
+++ middleware.js
@@ -187,7 +187,7 @@ const setupMiddleware = (app) => {
   app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'x-user-currency', 'x-user-country'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-user-currency', 'x-user-country', 'x-user-id'],
   }));
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ extended: true }));
